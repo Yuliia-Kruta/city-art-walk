58b4e1a745e9c912ce2312da7905b52449f57989
Yuliia Kruta <130898660+Yuliia-Kruta@users.noreply.github.com>
2024-11-05 20:46:01 +1000

    Initial list fragment added

---
+++ app/src/main/java/com/example/csc202assignment/ArtworkListFragment.kt
@@ -0,0 +1,43 @@
+package com.example.csc202assignment
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.csc202assignment.databinding.FragmentArtworkListBinding
+
+class ArtworkListFragment: Fragment() {
+
+    private var _binding: FragmentArtworkListBinding? = null
+    private val binding
+        get() = checkNotNull(_binding) {
+            "Cannot access binding because it is null. Is the view visible?"
+        }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentArtworkListBinding.inflate(inflater, container, false)
+
+        binding.artworkRecyclerView.layoutManager = LinearLayoutManager(context)
+
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
